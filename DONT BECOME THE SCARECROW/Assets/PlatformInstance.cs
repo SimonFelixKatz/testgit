b0592c0218008d2bdb1f8f861684be16f447c7eb
SimonFelixKatz <Simon.katz@icloud.com>
2017-08-13 21:05:22 +0200

    First Commit
    Hello v1

---
+++ DONT BECOME THE SCARECROW/Assets/PlatformInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInstance : MonoBehaviour {
+	[SerializeField]
+	GameObject[] obstacles;
+	[SerializeField]
+	Transform[] obstaclePositions;
+	// Use this for initialization
+	void Start () {
+		GameObject.Instantiate (obstacles [Random.Range (0, obstacles.Length)], obstaclePositions[Random.Range(0,obstaclePositions.Length)].position,Quaternion.identity);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DONT BECOME THE SCARECROW/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	[SerializeField]
+	Transform start;
+	[SerializeField]
+	GameObject[] platforms;
+	[SerializeField]
+	float platformSize = 0f;
+	[SerializeField]
+	int platformNumber = 5;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < platformNumber; i++) {
+			GameObject.Instantiate (platforms [Random.Range (0, platforms.Length)], start.position + new Vector3 (10f * i, 0f, 0f),Quaternion.identity);
+		}
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
